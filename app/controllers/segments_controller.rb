05371174fbb4fa3a33ec8f8201f2c692dee738b8
Julia Krebs <152524036+jcrabs@users.noreply.github.com>
2024-09-03 12:08:33 +0200

    Can draw on the map and save the created coordinates in the database

--- app/controllers/segments_controller.rb
+++ app/controllers/segments_controller.rb
@@ -14,4 +14,27 @@ class SegmentsController < ApplicationController
 
     redirect_to map_path(@map)
   end
+
+  def import_drawing
+    @map = Map.find(params[:map_id])
+    # import coordinates from drawn routes and save them in the database
+    saveok = []
+    params["coordinates"].each do |coords|
+      @segment = Segment.create(map: @map)
+      coords.each do |pair|
+        @point = Point.new(lat: pair["lat"], lon: pair["lon"], segment: @segment)
+        saveok << @point.save
+      end
+    end
+
+    # send response back to frontend
+    respond_to do |format|
+      if saveok.all?
+        format.json { render json: { success: true } }
+      else
+        # format.json { render json: { success: false, errors: @point.errors.full_messages }, status: :unprocessable_entity }
+        format.json { render json: { success: false }, status: :unprocessable_entity }
+      end
+    end
+  end
 end
