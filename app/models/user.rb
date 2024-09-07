39ac48a7ba54972614d64479b6de56de801781bc
Julia Krebs <152524036+jcrabs@users.noreply.github.com>
2024-09-08 00:40:06 +0200

    Fixed duplicated login link; added default avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,17 @@ class User < ApplicationRecord
   has_many :maps
   has_many :points_of_interest
   has_many :explored_points_of_interest
+
+  def avatar_tag
+    if photo.attached?
+      return ActionController::Base.helpers.cl_image_tag(photo.key, alt: "avatar",
+             class: "avatar-bordered dropdown-toggle", id: "navbarDropdown",
+             data:{bs_toggle: "dropdown"}, aria_haspopup: "true", aria_expanded: "false")
+    else
+      return ActionController::Base.helpers.image_tag("pallas_small.png", alt: "avatar",
+             class: "avatar-bordered dropdown-toggle", id: "navbarDropdown",
+             data:{bs_toggle: "dropdown"}, aria_haspopup: "true", aria_expanded: "false")
+    end
+  end
+
 end
